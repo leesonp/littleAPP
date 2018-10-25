86361caf79264a46107a41a2738a54cf877e9e96
leesonp <512523045@qq.com>
2018-10-25 17:34:41 +0800

    Update modelTest.js
    修复模板不滑动，点击确定没有返回数据的问题

--- pages/modelTest/modelTest.js
+++ pages/modelTest/modelTest.js
@@ -22,18 +22,29 @@ Page({
   },
   //隐藏picker-view
   hiddenFloatView: function (e) {
+    console.log("id = " + e.target.dataset.id)
     model.animationEvents(this, 200, false,400);
+    //点击确定按钮更新数据(id=444是背后透明蒙版 id=555是取消按钮)
+    if (e.target.dataset.id == 666){
+      this.updateShowData()
+    }
   },
   //滑动事件
   bindChange: function (e) {
     model.updateAreaData(this, 1, e);
+    //如果想滑动的时候不实时更新，只点确定的时候更新，注释掉下面这行代码即可。
+    this.updateShowData()
+  },
+  //更新顶部展示的数据
+  updateShowData:function(e){
     item = this.data.item;
     this.setData({
       province: item.provinces[item.value[0]].name,
       city: item.citys[item.value[1]].name,
       county: item.countys[item.value[2]].name
     });
-  },
+  }
+  ,
   onReachBottom: function (){
   },
   nono: function (){}
